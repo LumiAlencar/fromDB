11103edcabd0ed337a8da83f1b3904e298719798
LumiAraujo <levi.de.alencar.araujo@gmail.com>
2023-11-10 17:55:26 -0300

    dual search system

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import SearchID from './SearchID.jsx'
 import SearchName from './SearchName.jsx'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Welcome to FromDB</h1>
+    <h1>Welcome to <span>FromDB</span></h1>
     <h3>be our guest and search for a name or an ID...</h3>
-    
-    <SearchName/>
+    <div className="grid">
+      <div className='card'>
+        <SearchName/>
+      </div>
+      <div className="card">
+        <SearchID />
+      </div>
+    </div>
     </>
   )
 }
--- src/SearchID.jsx
+++ src/SearchID.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { useState } from "react";
 
 const SearchID = () => {
-    const [ searchOutput, setSearchOutput ] = useState({})
+    const [ searchOutputID, setSearchOutputID ] = useState({})
 
     return (
         <>
         <h2>Search user by ID:</h2>
-        <input type="number" name="ID Input" id="1" placeholder="0" />
+        <input type="number" name="ID Input" id="2" placeholder="0" />
         <button onClick={() => {
-            let search = document.getElementById("1").value
-            fetch(`http://localhost:3000/clients/${search}`).then(result => result.json()).then(json => {setSearchOutput(json)})
+            let search = document.getElementById("2").value
+            fetch(`http://localhost:3000/clients/${search}`).then(result => result.json()).then(json => {setSearchOutputID(json)})
         }}>Search</button>
         <div className="output">
-            <p>ID: {searchOutput.id}</p>
-            <p>Name: {searchOutput.name}</p>
-            <p>Username: {searchOutput.username}</p>
+            <p>ID: {searchOutputID.id}</p>
+            <p>Name: {searchOutputID.name}</p>
+            <p>Username: {searchOutputID.username}</p>
         </div>
         </>
     )
